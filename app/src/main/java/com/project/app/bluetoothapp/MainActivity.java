c76c5148c5a75c13b5fdd7af7f9f18799d2e3d4d
Rizal_Muhamad_H <rizalmuhamadh@gmail.com>
2017-01-25 10:33:17 +0700

    Paired Devices

--- app/src/main/java/com/project/app/bluetoothapp/MainActivity.java
+++ app/src/main/java/com/project/app/bluetoothapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.project.app.bluetoothapp;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button turnOn;
     private Button turnOff;
     private Button disc;
+    private Button refresh;
 
     private BluetoothAdapter bluetoothAdapter;
     private Set<BluetoothDevice>device;
@@ -32,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         turnOn = (Button)findViewById(R.id.turn_on);
         turnOff = (Button)findViewById(R.id.turn_off);
         disc = (Button)findViewById(R.id.discoverable);
+        refresh = (Button)findViewById(R.id.refresh);
         list = (ListView)findViewById(R.id.list_id);
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -39,6 +44,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         turnOn.setOnClickListener(this);
         turnOff.setOnClickListener(this);
         disc.setOnClickListener(this);
+        refresh.setOnClickListener(this);
     }
 
     @Override
@@ -50,7 +56,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Toast.makeText(this, "Turning On Bluetooth", Toast.LENGTH_LONG).show();
 
                     startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE),0);
-                    listId();
                 }
                 break;
             case R.id.turn_off:
@@ -64,16 +69,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE),0);
                 }
                 break;
+            case R.id.refresh :
+                if (bluetoothAdapter.isEnabled()){
+                    foundDevices();
+                }
+                break;
         }
     }
-    private void listId(){
+    private void foundDevices(){
         device = bluetoothAdapter.getBondedDevices();
 
-        ArrayList arrayList = new ArrayList();
+        final ArrayList<String> arrayList = new ArrayList();
 
         for (BluetoothDevice bluetoothDevice : device){
             arrayList.add(bluetoothDevice.getName());
         }
+        final BroadcastReceiver receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (BluetoothDevice.ACTION_FOUND.equals(action)){
+                    BluetoothDevice bluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                    arrayList.add(bluetoothDevice.getName());
+                }
+            }
+        };
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver,filter);
         if (arrayList.isEmpty()){
             Toast.makeText(this, "Paired Not Device", Toast.LENGTH_SHORT).show();
         }   else {
@@ -82,5 +105,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
 
         list.setAdapter(adapter);
+    }
+
+
+
+    @Override
+    protected void onDestroy() {
+//        unregisterReceiver(receiver);
+        super.onDestroy();
     }
 }
